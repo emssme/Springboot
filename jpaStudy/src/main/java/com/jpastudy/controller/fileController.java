a8962eac74146ae98a25c4e65f923f4a7b810959
emssme <bk75121@gmail.com>
2023-12-06 13:34:35 +0900

    file upload 완료

--- jpaStudy/src/main/java/com/jpastudy/controller/fileController.java
+++ jpaStudy/src/main/java/com/jpastudy/controller/fileController.java
@@ -2,19 +2,97 @@ package com.jpastudy.controller;
 
 
 import com.jpastudy.dto.UploadFileDTO;
+import com.jpastudy.dto.UploadResultDTO;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 @RestController
 @Log4j2
-public class fileController {
-    @ApiOperation(value = "Upload POST", notes = "POST 방식으로 등록")
+public class FileController {
+
+    @Value("${org.zerock.upload.path}")     // application.properties 에 설정한 값 ( C:\\upload )
+    private String uploadPath;              // 나중에 파일을 업로드하는 경로
+
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public String upload(UploadFileDTO uploadFileDTO) {
+    public List<UploadResultDTO> upload(UploadFileDTO uploadFileDTO){
         log.info(uploadFileDTO);
+        if(uploadFileDTO.getFiles() != null){
+            final List<UploadResultDTO> list = new ArrayList<>();
+            uploadFileDTO.getFiles().forEach(multipartFile -> {
+                String originalName = multipartFile.getOriginalFilename();
+                log.info(originalName);
+                String uuid = UUID.randomUUID().toString();
+                Path savePath = Paths.get(uploadPath, uuid+"_"+ originalName);
+                boolean image = false;
+                try {
+                    multipartFile.transferTo(savePath);
+                    if(Files.probeContentType(savePath).startsWith("image")){                               // savePath 에 저장된 파일이 이미지가 맞다면
+                        image = true;                                                                       // image 플래그를 true로 설정
+                        File thumbFile = new File(uploadPath, "s_" + uuid+"_"+ originalName);          // 썸네일 이미지 저장명 : s_uuid_originalName
+                        Thumbnailator.createThumbnail(savePath.toFile(), thumbFile, 200,200);   // savePath 에 썸네일 이미지를 200 * 200 사이즈로 저장
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                list.add(UploadResultDTO.builder()
+                        .uuid(uuid)
+                        .fileName(originalName)
+                        .img(image).build()
+                );
+            });
+            return list;
+        }
         return null;
+    }
+
+    @GetMapping("/view/{fileName}")
+    public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName){
+        Resource resource = new FileSystemResource(uploadPath+File.separator + fileName);
+        String resourceName = resource.getFilename();
+        HttpHeaders headers = new HttpHeaders();
+        try{
+            headers.add("Content-Type", Files.probeContentType( resource.getFile().toPath() ));
+        } catch(Exception e){
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok().headers(headers).body(resource);
+    }
+
+    @DeleteMapping("/remove/{fileName}")
+    public Map<String,Boolean> removeFile(@PathVariable String fileName){
+        Resource resource = new FileSystemResource(uploadPath+File.separator + fileName);
+        String resourceName = resource.getFilename();
+        Map<String, Boolean> resultMap = new HashMap<>();
+        boolean removed = false;
+        try {
+            String contentType = Files.probeContentType(resource.getFile().toPath());
+            removed = resource.getFile().delete();
+            //섬네일이 존재한다면
+            if(contentType.startsWith("image")){
+                File thumbnailFile = new File(uploadPath+File.separator +"s_" + fileName);
+                thumbnailFile.delete();
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        resultMap.put("result", removed);
+        return resultMap;
     }
 }
